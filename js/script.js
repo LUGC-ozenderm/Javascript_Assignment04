a846301ff740cbf4167561f687733340979a36d8
olegz <ozenderm@lakeahu.ca>
2024-04-08 10:09:36 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   css/style.css
    	new file:   index.html
    	new file:   js/script.js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+async function findThings() {
+    const apiKey = '4fa16c23'; 
+    const movie = document.getElementById('searchInput').value;
+
+    const response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${apiKey}`);
+    const data = await response.json();
+
+    if (data.Response === 'False') {
+      alert(data.Error);
+      return;
+    }
+
+    const things = [
+      data.Title,
+      data.Plot,
+      data.Actors,
+      data.Genre,
+      data.Director
+    ];
+
+    const output = document.getElementById('output');
+    output.innerHTML = '';
+
+    things.forEach((thing, index) => {
+      const div = document.createElement('div');
+      div.textContent = `${index + 1}. ${thing}`;
+      div.classList.add('animation');
+      output.appendChild(div);
+    });
+
+    const poster = document.getElementById('poster');
+    poster.src = data.Poster;
+    poster.style.display = 'block';
+  }
